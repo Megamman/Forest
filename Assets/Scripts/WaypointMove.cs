62a00b604a2575eb7b62faa70c52b7616efb17e3
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2019-04-26 19:48:06 +0200

    Main menu

---
+++ Assets/Scripts/WaypointMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMove : MonoBehaviour
+{
+
+    public float speed;
+
+    public Transform[] waypoints;
+    private int randomSpot;
+
+    public float startWaitTime;
+    private float waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, waypoints.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, waypoints[randomSpot].position) < 0.2f)
+        {
+            if( waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, waypoints.Length);
+                waitTime = startWaitTime;
+            } else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
