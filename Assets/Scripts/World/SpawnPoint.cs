cb113fe1be778791aa6d5ae5a6703e0ac604de88
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2019-04-29 21:14:13 +0200

    Level 2 with teemplates

---
+++ Assets/Scripts/World/SpawnPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+
+    public GameObject[] enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, enemies.Length);
+        Instantiate(enemies[rand], transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
